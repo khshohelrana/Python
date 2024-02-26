f293bd3176c7a2d639479f8ecd2877405ca53ebf
Kh. Shohel Rana <khshohelrana021@gmail.com>
2024-02-27 01:58:29 +0600

    first commit

---
+++ Firstpython_practice.py
@@ -0,0 +1,21 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+# plotting to our canvas
+plt.plot([1, 2.5, 3, 4.5], [1, 2, 3.5, 4.5])
+
+# showing what we plotted
+plt.show()
+
+
+
+# plotting to our canvas
+plt.plot([1, 2.5, 3, 4.5], [1, 2, 3.5, 4.5])
+
+# adding title and axis labels
+plt.title("Title of the plot")
+plt.xlabel("X axis")
+plt.ylabel("Y axis")
+
+# showing what we plotted
+plt.show()
